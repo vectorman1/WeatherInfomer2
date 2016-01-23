dc194612f3472256953a6f6eaf0b817d3c86ae0a
vectorman1 <boris.lachevbg00@gmail.com>
2016-01-23 12:53:14 +0200

    Initial Commit

---
+++ wear/src/main/java/tk/lachev/weatherinformer2/MyStubBroadcastActivity.java
@@ -0,0 +1,21 @@
+package tk.lachev.weatherinformer2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Example shell activity which simply broadcasts to our receiver and exits.
+ */
+public class MyStubBroadcastActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = new Intent();
+        i.setAction("tk.lachev.weatherinformer2.SHOW_NOTIFICATION");
+        i.putExtra(MyPostNotificationReceiver.CONTENT_KEY, getString(R.string.title));
+        sendBroadcast(i);
+        finish();
+    }
+}
