e6a86029aa85593926dc52764cca999a70564ecd
vectorman1 <boris.lachevbg00@gmail.com>
2016-01-23 14:33:05 +0200

    Added class for Fetching from API.

---
+++ mobile/src/main/java/tk/lachev/weatherinformer2/utils/RemoteFetch.java
@@ -0,0 +1,54 @@
+package tk.lachev.weatherinformer2.utils;
+
+import android.content.Context;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import tk.lachev.weatherinformer2.R;
+
+/**
+ * Created by Boris Lachev on 1/23/2016.
+ */
+public class RemoteFetch {
+    private static final String OPEN_WEATHER_MAP_API =
+            "http://api.openweathermap.org/data/2.5/weather?q=%s&units=metric";
+
+    public static JSONObject getJSON(Context context, String city) {
+        try {
+            URL url = new URL(String.format(OPEN_WEATHER_MAP_API, city));
+            HttpURLConnection connection =
+                    (HttpURLConnection) url.openConnection();
+
+            connection.addRequestProperty("x-api-key",
+                    context.getString(R.string.open_weather_maps_app_id));
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            StringBuffer json = new StringBuffer(1024);
+
+            String tmp = "";
+
+            while ((tmp = reader.readLine()) != null) {
+                json.append(tmp).append("\n");
+            }
+            reader.close();
+            JSONObject data = new JSONObject(json.toString());
+
+            if (data.getInt("cod") != 200) {
+                return null;
+            }
+            return data;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
